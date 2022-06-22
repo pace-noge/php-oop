5ea4a96d349a57365e8d5a3ccecbda738ff229e1
Nasaruddin <nasaruddin@mekari.com>
2022-06-22 09:13:46 +0700

    php objects

---
+++ php_objects.php
@@ -0,0 +1,33 @@
+<?php
+
+class BankAccount
+{
+    public $accountNumber;
+    public $balance;
+
+    public function deposit($amount)
+    {
+        if ($amount > 0) {
+            $this->balance += $amount;
+        }
+    }
+
+    public function withdraw($amount)
+    {
+        if ($amount <= $this->balance) {
+            $this->balance -= $amount;
+        }
+    }
+
+}
+
+$account = new BankAccount();
+
+$account->accountNumber = 1;
+$account->balance = 100;
+$account->deposit(100);
+$account->withdraw(50);
+
+echo($account->balance);
+
+?>
